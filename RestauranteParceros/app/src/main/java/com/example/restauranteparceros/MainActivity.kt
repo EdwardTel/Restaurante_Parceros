b7215a9b7619ab2628f2cf59b0690f9d6021c5f7
EdwardTel <edward_tel@yahoo.es>
2022-10-18 10:33:21 -0500

    Registro y Login enlazados a Firebase

--- RestauranteParceros/app/src/main/java/com/example/restauranteparceros/MainActivity.kt
+++ RestauranteParceros/app/src/main/java/com/example/restauranteparceros/MainActivity.kt
@@ -5,26 +5,64 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var btnLogin : Button
     private lateinit var btnRegistro : Button
-    private lateinit var etCorreo : EditText
+    private lateinit var etEmail : EditText
     private lateinit var etPass : EditText
+
+    private lateinit var firebaseAuth : FirebaseAuth
+    private lateinit var authStateListener : FirebaseAuth.AuthStateListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnLogin = findViewById(R.id.login_button_login)
         btnRegistro = findViewById(R.id.login_button_registrar)
-        etCorreo = findViewById(R.id.login_et_correo)
+        etEmail = findViewById(R.id.login_et_email)
         etPass = findViewById(R.id.login_et_pass)
 
-        btnRegistro.setOnClickListener {
+        firebaseAuth = Firebase.auth
 
+        btnLogin.setOnClickListener {
+            if(etEmail.text.isEmpty() || etPass.text.isEmpty()){
+                Toast.makeText(baseContext,"Inválido, hay campos vacíos", Toast.LENGTH_SHORT).show()
+            }
+
+            else{
+                var userEmail = etEmail.text.toString()
+                var userPass = etPass.text.toString()
+                singIn(userEmail,userPass)
+            }
+        }
+
+        btnRegistro.setOnClickListener {
             val i = Intent(this, RegistroActivity::class.java)
             startActivity(i)
         }
+    }
 
+    private fun singIn(email: String,  password: String ){
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){
+            if(it.isSuccessful){
+                val user = firebaseAuth.currentUser
+                Toast.makeText(baseContext,user?.uid.toString(), Toast.LENGTH_SHORT).show()
+                Toast.makeText(baseContext,"Logueado Correctamente!", Toast.LENGTH_SHORT).show()
+
+                //Aqui se inicia el siguiente activity
+            }
+
+            else{
+                Toast.makeText(baseContext,"Correo o contraseña no válidos", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- RestauranteParceros/app/src/main/java/com/example/restauranteparceros/RegistroActivity.kt
+++ RestauranteParceros/app/src/main/java/com/example/restauranteparceros/RegistroActivity.kt
@@ -5,21 +5,71 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegistroActivity : AppCompatActivity() {
 
+    private lateinit var etNombre : EditText
+    private lateinit var etApellido : EditText
+    private lateinit var etTelefono : EditText
+    private lateinit var etEmail : EditText
+    private lateinit var etPass : EditText
+
     private lateinit var btnRegistrarse : Button
+
+    private lateinit var firebaseAuth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
 
+        etNombre = findViewById(R.id.registro_et_nombre)
+        etApellido = findViewById(R.id.registro_et_apellido)
+        etTelefono = findViewById(R.id.registro_et_telefono)
+        etEmail = findViewById(R.id.registro_et_email)
+        etPass = findViewById(R.id.registro_et_password)
+
         btnRegistrarse = findViewById(R.id.registro_button_registrarse)
+
+        firebaseAuth = Firebase.auth
 
         btnRegistrarse.setOnClickListener {
 
-            this.finish();
+            if(etNombre.text.isEmpty() ||
+                etApellido.text.isEmpty() ||
+                etTelefono.text.isEmpty() ||
+                etEmail.text.isEmpty() ||
+                etPass.text.isEmpty()){
+
+                Toast.makeText(baseContext,"Inválido, hay campos vacíos", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                var userNombre = etNombre.text.toString()
+                var userApellido = etNombre.text.toString()
+                var userTelefono = etTelefono.text.toString()
+                var userEmail = etEmail.text.toString()
+                var userPass = etPass.text.toString()
+
+                createAccount(userNombre, userApellido, userTelefono, userEmail, userPass)
+            }
+        }
+    }
+
+    private fun createAccount(nombre: String, apellido: String, telefono: String, email: String, pass: String){
+
+        firebaseAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(this){
+            if(it.isSuccessful){
+                Toast.makeText(baseContext,"Creado Correctamente!", Toast.LENGTH_LONG).show()
+                this.finish()
+            }
+            else{
+                Snackbar.make(btnRegistrarse,"Error: ${it.exception}", Snackbar.LENGTH_LONG ).setTextMaxLines(5).show()
+            }
         }
     }
 }
